85d72df7ed8ecf0671943caccdf35b6d5b855c6b
rajasimhan varadharajan <rajasimhan.varadharajan@gmail.com>
2020-02-17 19:47:27 -0500

    initial commit

---
+++ src/test/java/com/rv/functional/interfaces/lambdas/LambdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rv.functional.interfaces.lambdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
